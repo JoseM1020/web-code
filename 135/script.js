0f57593570c3aebc25791fd289d16aa5a7057216
Jose Muniz <muniz0@purdue.edu>
2020-06-07 21:41:57 -0500

    updated to 140

--- 135/script.js
+++ 135/script.js
@@ -2,18 +2,29 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randBtn = document.querySelector(".random");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
+}
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function randomizeColor() {
+  color1.value = getRandomColor();
+  color2.value = getRandomColor();
+  setGradient();
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+randBtn.addEventListener("click", randomizeColor);
+
+window.onload = setGradient();
